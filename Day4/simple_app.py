8dcf86a4c72bc70143d44e2a501da4589c1a72eb
akshayagoddeti <akshayagoddeti5@gmail.com>
2026-09-26 11:47:12 +0530

    Day4:Simpleapp Day5:UI-ChatBot

--- Day4/simple_app.py
+++ Day4/simple_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 st.title("My first Streamlit App!!!")
 st.header("Welcome to my AI Application !")
+st.subheader("Akshaya")
+st.markdown("*Ammulu*")
 name= st.text_input("enter Your name:")
 if st.button("Submit"):
     st.write("hello",name)
---
+++ Day5/uiBot.py
@@ -0,0 +1,37 @@
+import ollama
+import streamlit as st
+st.title("Welcome to my ChatBot App!!")
+with st.sidebar:
+    uploaded_file = st.file_uploader("Upload a text file ...")
+    if uploaded_file:
+        st.write("File Uploaded succusfully")
+        content = uploaded_file.read().decode("utf-8")
+        st.write(content)
+if "messages" not in st.session_state:
+     st.session_state.messages=[]
+for msg in st.session_state.messages:
+     with st.chat_message(msg["role"]):
+             st.write(msg["content"])
+question=st.chat_input("Ask question :")
+if question:
+    st.session_state.messages.append(
+        {
+            "role":"user",
+            "content":question
+        }
+)
+    with st.chat_message("user"):
+        st.write(question)
+    with st.spinner("Thinking ..."):
+        response = ollama.chat(
+            model="llama3.2:3b",
+            messages=st.session_state.messages
+)
+    st.session_state.messages.append({
+        "role":"assistant",
+        "content":response["message"]["content"]}
+    )
+    with st.chat_message("assistant"):
+            st.write("AI:",response["message"]["content"])
+
+    
